418ebd5d5fc25b5cb6292c40e7ea8a517da8d48b
Gilad Naaman <gilad@naaman.io>
2022-12-21 08:33:55 +0200

    Added "encode.py" script in refimpl

---
+++ python-refimpl/encode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import json
+import sbson_refimpl
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('Convert a JSON document to an SBSON document')
+        print('Usage: encode.py <input_file> <output_file>')
+        sys.exit(1)
+    _progname, input_path, output_path = sys.argv
+    with open(input_path, 'r') as f:
+        data = json.load(f)
+    data = sbson_refimpl.encode(data)
+    with open(output_path, 'wb') as f:
+        f.write(data)
+
+
+
+if __name__ == '__main__':
+    main()
+
--- python-refimpl/sbson_refimpl.py
+++ python-refimpl/sbson_refimpl.py
@@ -175,37 +175,3 @@ def decode_array(view: memoryview) -> list:
     
     return items
 
-
-def generate_test_vectors():
-    sanity = {
-        '3': b"beep boop",
-        'BLARG': [1, 2, True, False, None],
-        'FLORP': {'X': 0xFF},
-        "help me i'm trapped in a format factory help me before they": "..."
-    }
-    goto = {
-        "top": {
-            f"item_{i:04}": {
-                "something": [100] * 100
-            } for i in range(8000)
-        }
-    }
-
-    vectors = {
-        "sanity": sanity,
-        "goto": goto,
-    }
-    for name, vector in vectors.items():
-        # with open(f"../test_vectors/{name}.json", "w") as f:
-        #     json.dump(f, vector)
-        print(f"Encoding {name}")
-        sbson = encode(vector)
-        print(f"Decoding {name}")
-        assert decode(sbson) == vector
-        print(f"Saving {name}.sbson")
-        with open(f"../test_vectors/{name}.sbson", "wb") as f:
-            f.write(sbson)
-
-
-if __name__ == '__main__':
-    generate_test_vectors()
